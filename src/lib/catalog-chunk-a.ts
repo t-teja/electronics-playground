9ae04999de1e3b5d9cdcc864c9de178c2e84f19d
Teja <202754519+t-teja@users.noreply.github.com>
2026-09-07 21:04:43 +0530

    Add thin catalog chunk A/B re-exports.

---
+++ src/lib/catalog-chunk-a.ts
@@ -0,0 +1,4 @@
+import { CATALOG_CHUNK_A1 } from "./catalog-chunk-a1";
+import { CATALOG_CHUNK_A2 } from "./catalog-chunk-a2";
+
+export const CATALOG_CHUNK_A = [...CATALOG_CHUNK_A1, ...CATALOG_CHUNK_A2];
---
+++ src/lib/catalog-chunk-b.ts
@@ -0,0 +1,4 @@
+import { CATALOG_CHUNK_B1 } from "./catalog-chunk-b1";
+import { CATALOG_CHUNK_B2 } from "./catalog-chunk-b2";
+
+export const CATALOG_CHUNK_B = [...CATALOG_CHUNK_B1, ...CATALOG_CHUNK_B2];
